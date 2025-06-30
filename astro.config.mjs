1905e68e8ecfc9574879025b3d970fe9f4f23a1b
xiaobei <beixiaosheng@163.com>
2025-06-30 10:17:21 +0800

    add about and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://wigglypaint.com',
+    integrations: [
+        sitemap({
+            i18n: {
+                defaultLocale: 'en',
+                locales: {
+                    en: 'en',
+                    zh: 'zh-CN',
+                    ja: 'ja',
+                    ko: 'ko'
+                }
+            },
+            entryLimit: 50000  // 设置更高的限制，避免分割成多个sitemap
+        })
+    ],
     i18n: {
         locales: ['en', 'zh', 'ja', 'ko'],
         defaultLocale: 'en',
